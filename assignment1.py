611a1f3cd5169cd412358ff7c7c89d0e7c7115fc
costantino pacilio <costantinopacilio7@hotmail.it>
2019-01-21 22:17:42 +0100

    irrational branch

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,6 @@
 import sys #to import from command line
 import matplotlib.pyplot as plt #to plot
+import math as mt
 
 #Usage message
 try:
@@ -7,6 +8,8 @@ try:
 except:
     print("Usage: give one of the following command line arguments:")
     print("1: f(x) = x")
+    print("5: f(x) = exp(x)")
+    print("6: f(x) = sqrt(abs(x))")
     sys.exit()
     
 #initialize list
@@ -15,6 +18,13 @@ xval = [i*0.1 for i in range(-50,51)]
 #implement function
 if n == "1":
     yval = [x for x in xval]
+elif n == "5":
+    yval = [mt.exp(x) for x in xval]
+elif n == "6":
+    yval = [mt.sqrt(mt.fabs(x)) for x in xval]
+else:
+    print("Invalid argument")
+    sys.exit()
     
 #plot
 plt.figure()
