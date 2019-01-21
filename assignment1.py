6893a481ec54b3be6e84b4ad9aa12b51a4bc0d4c
costantino pacilio <costantinopacilio7@hotmail.it>
2019-01-21 21:40:09 +0100

    Fill lists

--- assignment1.py
+++ assignment1.py
@@ -2,4 +2,12 @@ import sys #to import from command line
 
 n = sys.argv[1] #read command line input
 
-print(n)
+#initialize list
+xval = [i*0.1 for i in range(-50,51)]
+
+#implement function
+if n == "1":
+    yval = [x for x in xval]
+
+print(yval)
+
