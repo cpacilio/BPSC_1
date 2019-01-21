17771a6293d68c6320d5d8194fdc64d9f3973b10
costantino pacilio <costantinopacilio7@hotmail.it>
2019-01-21 21:56:47 +0100

    polynomial

--- assignment1.py
+++ assignment1.py
@@ -9,7 +9,11 @@ xval = [i*0.1 for i in range(-50,51)]
 #implement function
 if n == "1":
     yval = [x for x in xval]
-
+if n == "2":
+    yval = [x**2 for x in xval]
+if n == "3":
+    yval = [x**3 for x in xval]
+    
 #plot
 plt.figure()
 plt.plot(xval,yval)
