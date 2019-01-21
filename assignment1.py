c313ddedce865899df078ade0f5ee61a6acb39e5
costantino pacilio <costantinopacilio7@hotmail.it>
2019-01-21 22:04:19 +0100

    trigonometric

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,6 @@
 import sys #to import from command line
 import matplotlib.pyplot as plt #to plot
+import math as mt
 
 n = sys.argv[1] #read command line input
 
@@ -9,6 +10,12 @@ xval = [i*0.1 for i in range(-50,51)]
 #implement function
 if n == "1":
     yval = [x for x in xval]
+if n == "2":
+    yval = [mt.sin(x) for x in xval]
+if n == "3":
+    yval = [mt.cos(x) for x in xval]
+if n == "4":
+    yval = [mt.tan(x) for x in xval]
     
 #plot
 plt.figure()
